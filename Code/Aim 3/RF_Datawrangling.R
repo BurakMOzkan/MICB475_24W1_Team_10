62aa638e09dc2e563a8bc3302630c4d83ee715eb
aoctoman <99299029+aoctoman@users.noreply.github.com>
2024-12-18 17:26:39 -0800

    Update RF_Datawrangling.R

--- Code/Aim 3/RF_Datawrangling.R
+++ Code/Aim 3/RF_Datawrangling.R
@@ -6,7 +6,7 @@ library(dplyr)
 
 phylo <- MSphylo_filt
 
-
+#Determine list of family/genus we want to incorporate into 
 family_of_interest <- c("f__Akkermansiaceae", "f__Peptostreptococcales-Tissierellales", 
                         "f__Butyricicoccaceae", "f__Oscillospiraceae")
 
@@ -14,8 +14,10 @@ genus_of_interest <- c("g__Oscillibacter", "g__Akkermansia", "g__Faecalibacteriu
                        "g__Erysipelotrichaceae_UCG", "g__Roseburia", "g__Christensenellaceae_R-7_group", 
                        "g__Blautia", "g__Fusicatenibacter", "g__Agathobacter", "g__Frisingicoccus")
 
+#Create table of metadata
 metadata_table <- phylo@sam_data
 
+#Create dataframe of genus/family values
 ps_for_rf_genus = phylo %>% 
   microbiome::transform('clr') %>%  # Works on phyloseq objects
   subset_taxa(Genus %in% genus_of_interest) %>%
@@ -32,10 +34,8 @@ ps_for_rf_family = phylo %>%
   pivot_wider(names_from = Family, values_from = Abundance) %>%
   select(Sample, contains('f__'))
 
+#Combine family and genus level data together
 combined <- left_join(ps_for_rf_genus, ps_for_rf_family, by = "Sample") 
 
-ps_for_rf_genus <- sapply(ps_for_rf_genus$list_column, `[`, 1)
-
-ps_for_rf_genus <- as.data.frame(ps_for_rf_genus)
-
+#Save File 
 write.csv(ps_for_rf_family, file = "MS_dataforRF_allgenusallage.csv", row.names = FALSE)
